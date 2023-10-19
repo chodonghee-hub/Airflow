6ba3172fe9ad28b5044458be7a526bb9f97bdab3
chodonghee-hub <67031025+chodonghee-hub@users.noreply.github.com>
2023-10-19 10:58:07 +0900

    bash_operator dag

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -1,24 +1,22 @@
 from airflow import DAG
-# from airflow.operators.bash import BashOperator
+from airflow.operators.bash_operator import BashOperator
 import datetime
 import pendulum
 
 with DAG(
-    dag_id="dags_bash_operator",                                # DAG ID 
-    schedule="0 0 * * *",                                       # 분 시 일 월 요일    
-    start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),  # 시간 기준
+    dag_id="dags_bash_operator",                              
+    schedule="0 0 * * *",                                     
+    start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),
     catchup=False,
-    #dagrun_timeout=datetime.timedelta(minutes=60),
 ) as dag : 
     bash_t1 = BashOperator(
-        task_id="bash_t1",                                      # Task 이름 
+        task_id="bash_t1",                                    
         bash_command="echo whoami",
     )
 
     bash_t2 = BashOperator(
-        task_id="bash_t2",                                      # Task 이름 
-        bash_command="echo $HOSTNAME",                          # WSL HOSTNAME
+        task_id="bash_t2",                                    
+        bash_command="echo $HOSTNAME",                        
     )
 
-    bash_t1 >> bash_t2                                          # 수행 순서 
-
+    bash_t1 >> bash_t2                                        

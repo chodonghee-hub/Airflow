744c5a745c4e41c09a6349055491cb2b7de22585
chodonghee-hub <67031025+chodonghee-hub@users.noreply.github.com>
2023-10-23 21:46:40 +0900

    dag_example

---
+++ dags/dag_simple_example.py
@@ -0,0 +1,31 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+import pendulum
+
+def hello_airflow() : 
+    print("hello_airflow")
+
+with DAG(
+    dag_id="DAG_Simple_Example",                                
+    schedule="0 0 * * *",                                       
+    schedule_interval="@daily",                                 
+    start_date=pendulum.datetime(2023, 10, 1, tz="Asia/Seoul"), 
+    catchup=False,                                              
+) as dag : 
+    op_start = BashOperator(
+        task_id="op_start",
+        bash_command="start_point",
+    )
+
+    op_print_msg = PythonOperator(
+        task_id="op_print_msg",
+        python_callable=hello_airflow,
+    )
+
+    op_end = BashOperator(
+        task_id="op_end",
+        bash_command="complete",
+    )
+
+    op_start >> op_print_msg >> op_end

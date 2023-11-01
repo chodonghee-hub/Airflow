a9d098c2da9bc39014fd65ec29408a2169929185
chodonghee-hub <67031025+chodonghee-hub@users.noreply.github.com>
2023-11-01 22:33:37 +0900

    updated

--- dags/mcDonalds.py
+++ dags/mcDonalds.py
@@ -7,7 +7,7 @@ from airflow.utils.trigger_rule import TriggerRule
 import pendulum
 
 with DAG(
-    dag_id="Hamburger",
+    dag_id="McDrive",
     schedule="0 0 * * *",
     start_date=pendulum.datetime(2023, 1, 1, tz="Asia/Seoul"),
     catchup=False,
@@ -47,6 +47,40 @@ with DAG(
         bash_command='echo "Onion ready"'
     )
 
+    #####################################################
+    #                  Drive Through                    #
+    #####################################################
+    op_visit = BashOperator(
+        task_id="visit",
+        bash_command='echo "McDonald Visit"'
+    )
+
+    op_order = BashOperator(
+        task_id="order",
+        bash_command='echo "Order"'
+    )
+
+    op_pay = BashOperator(
+        task_id="pay",
+        bash_command='echo "Pay"'
+    )
+
+    op_takeOut = BashOperator(
+        task_id="take_out",
+        bash_command='echo "Take Out"'
+    )
+
+    op_pay = BashOperator(
+        task_id="pay",
+        bash_command='echo "Pay"'
+    )
+
+    op_finish = BashOperator(
+        task_id="finish",
+        bash_command='echo "Pay"'
+    )
+
+    op_visit >> op_order >> op_pay >> op_bread
     op_bread >> op_patty >> op_sauce >> op_lettuce >> op_pickle >> op_cheeze >> op_onion
 
 
